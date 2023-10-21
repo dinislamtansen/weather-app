6e92f1d9daea7582b8cdfd22c9b180287d3daf95
tansen2425 <dinislamtansen11@gmail.com>
2023-10-21 23:48:51 +0600

    full done

--- js/app.js
+++ js/app.js
@@ -1,17 +1,24 @@
 
 const searchbtn= document.getElementById("searchbtn");
+ 
+const cityName=document.getElementById("cityname").innerText;
+
 
 const fetchWeather  = async (recvuserData) => {
     const weatherData= await fetch (
         `https://api.openweathermap.org/data/2.5/weather?q=${recvuserData}&appid=8dd5d8dbedf8cc015073642042e8d9ba`
     );
     const result=await weatherData.json();
-    console.log(result.name);
+    console.log(result);
+
+  const temperature= (result.main.temp-273.5).toFixed(2);
+  document.getElementById("tempR").innerText=temperature;
 }
 const getdata = () =>{
-    const userData = document.getElementById("searchbtn").value;
-    document.getElementById("searchbtn").value="";
-    fetchWeather(userData);
+    const userData = document.getElementById("inputBtn").value;
+        document.getElementById("inputBtn").value="";
+        document.getElementById("cityname").innerText= userData;
+        fetchWeather(userData);
  }
 searchbtn.addEventListener("click", getdata);
 
